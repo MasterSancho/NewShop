e85dea86afab597c03c24019ee7c74fd34b7e8aa
Alex M <1maltsevalex@gmail.com>
2020-12-21 11:09:31 +0200

    047-Front End User Authentication & Profile
    => Create Order

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { createOrder } from '../actions/orderActions';
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+ const dispatch = useDispatch();
+
  const cart = useSelector((state) => state.cart);
 
  // Calculate prices
@@ -16,22 +19,37 @@ const PlaceOrderScreen = () => {
  cart.itemsPrice = addDecimals(
   cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
  );
-
  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
-
  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
-
  cart.totalPrice = (
   Number(cart.itemsPrice) +
   Number(cart.shippingPrice) +
   Number(cart.taxPrice)
  ).toFixed(2);
 
- const placeOrderHandler = () => {
-  console.log('Hello');
- };
+ const orderCreate = useSelector((state) => state.orderCreate);
+ const { order, success, error } = orderCreate;
 
- const error = 'HEY';
+ useEffect(() => {
+  if (success) {
+   history.push(`/order/${order._id}`);
+  }
+  // eslint-disable-next-line
+ }, [history, success]);
+
+ const placeOrderHandler = () => {
+  dispatch(
+   createOrder({
+    orderItems: cart.cartItems,
+    shippingAddress: cart.shippingAddress,
+    paymentMethod: cart.paymentMethod,
+    itemsPrice: cart.itemsPrice,
+    shippingPrice: cart.shippingPrice,
+    taxPrice: cart.taxPrice,
+    totalPrice: cart.totalPrice,
+   })
+  );
+ };
 
  return (
   <>
@@ -47,13 +65,11 @@ const PlaceOrderScreen = () => {
         {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
        </p>
       </ListGroup.Item>
-
       <ListGroup.Item>
        <h2>Payment Method</h2>
        <strong>Method: </strong>
        {cart.paymentMethod}
       </ListGroup.Item>
-
       <ListGroup.Item>
        <h2>Order Items</h2>
        {cart.cartItems.length === 0 ? (
@@ -80,46 +96,39 @@ const PlaceOrderScreen = () => {
       </ListGroup.Item>
      </ListGroup>
     </Col>
-
     <Col md={4}>
      <Card>
       <ListGroup variant='flush'>
        <ListGroup.Item>
         <h2>Order Summary</h2>
        </ListGroup.Item>
-
        <ListGroup.Item>
         <Row>
          <Col>Items</Col>
          <Col>${cart.itemsPrice}</Col>
         </Row>
        </ListGroup.Item>
-
        <ListGroup.Item>
         <Row>
          <Col>Shipping</Col>
          <Col>${cart.shippingPrice}</Col>
         </Row>
        </ListGroup.Item>
-
        <ListGroup.Item>
         <Row>
          <Col>Tax</Col>
          <Col>${cart.taxPrice}</Col>
         </Row>
        </ListGroup.Item>
-
        <ListGroup.Item>
         <Row>
          <Col>Total</Col>
          <Col>${cart.totalPrice}</Col>
         </Row>
        </ListGroup.Item>
-
        <ListGroup.Item>
         {error && <Message variant='danger'>{error}</Message>}
        </ListGroup.Item>
-
        <ListGroup.Item>
         <Button
          type='button'
